8330c836d573dce6c0f9735778c5416917514636
marineco <marinecolonge@gmail.com>
2017-03-17 12:20:12 +0100

    display speed

--- src/App.js
+++ src/App.js
@@ -7,19 +7,13 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-
+      speed: 20
     };
   }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>{this.state.speed} km/h</h1>
       </div>
     );
   }
