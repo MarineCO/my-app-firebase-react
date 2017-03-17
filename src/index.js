e08a99031da4ffa5ee95bc98dd2d0408a18ac61b
marineco <marinecolonge@gmail.com>
2017-03-17 12:08:34 +0100

    initialize firebase

--- src/index.js
+++ src/index.js
@@ -4,8 +4,17 @@ import App from './App';
 import './index.css';
 import * as firebase from 'firebase';
 
+var config = {
+	apiKey: "AIzaSyBhoUrNWErNekjJUoDIzOcGm2kPYi8ZaxU",
+	authDomain: "my-app-firebase-react.firebaseapp.com",
+	databaseURL: "https://my-app-firebase-react.firebaseio.com",
+	storageBucket: "my-app-firebase-react.appspot.com",
+	messagingSenderId: "884979448697"
+};
+firebase.initializeApp(config);
+
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+	<App />,
+	document.getElementById('root')
+	);
