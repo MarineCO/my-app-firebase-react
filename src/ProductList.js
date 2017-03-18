03b8800f0f1d0d7d24039f1c804074db6801dfc9
marineco <marinecolonge@gmail.com>
2017-03-18 18:46:38 +0100

    add product list

---
+++ src/ProductList.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import * as firebase from 'firebase';
+
+class ProductList extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			products: [
+				{
+					id: 1,
+					name: "Casque",
+					price: 150
+				},
+				{
+					id: 2,
+					name: "Ordinateur",
+					price: 1300
+				},
+				{
+					id: 3,
+					name: "Chargeur",
+					price: 30 
+				}
+			]
+		}
+	}
+	render() {
+		const productList = this.state.products.map(product => {
+			return <li key={ product.id }> { product.name } : { product.price }</li>
+		});
+		return (
+			<div>
+				<ul>{ productList }</ul>
+			</div>
+		)
+	}
+}
+
+export default ProductList
